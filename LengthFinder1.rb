7de237c253706ba2670a58a14593e7adbfdd7641
aliasgarS <alisaif.5253@gmail.com>
2016-03-07 13:29:13 +0400

    Added files via upload

---
+++ LengthFinder1.rb
@@ -0,0 +1,7 @@
+def length_finder(input_array)
+	inputA = []
+	input_array.each do |x|
+		inputA.push(x.length)
+	end
+	inputA
+end
---
+++ LengthFinder2.rb
@@ -0,0 +1,4 @@
+def length_finder(input_array)
+	inputA = input_array.collect{|x| x.length}
+	inputA
+end
